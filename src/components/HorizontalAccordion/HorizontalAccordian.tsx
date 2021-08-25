f1e90338dd48dd0fe72c75f3eade0650890c95cc
Thumster <bryan.thum@hotmail.com>
2021-08-25 21:23:09 +0800

    updated mobile view for experience tab

--- src/components/HorizontalAccordion/HorizontalAccordian.tsx
+++ src/components/HorizontalAccordion/HorizontalAccordian.tsx
@@ -1,7 +1,12 @@
-import { useState, useEffect } from 'react';
+/* eslint-disable max-len */
+import React, { useState } from 'react';
 import Media from 'react-media';
-import { useInView } from 'react-intersection-observer';
-import { AnimatePresence } from 'framer-motion';
+import {
+	IoCaretBackOutline,
+	IoCaretForwardOutline,
+	IoCloseSharp,
+} from 'react-icons/io5';
+import { AnimatePresence, AnimateSharedLayout } from 'framer-motion';
 import {
 	SectionsMenu,
 	SectionContainer,
@@ -11,10 +16,13 @@ import {
 	SectionName,
 	ContentListContainer,
 	ContentListItemContainer,
-	StyledBackdrop,
 	SectionImageOutter,
 	OuterSectionContent,
 	MainContainer,
+	ArrowNavigation,
+	SSSectionContainer,
+	SSSectionImageContainer,
+	CloseButtonContainer,
 } from './HorizontalAccordianStyles';
 import { device } from '../../config/display';
 import { animateAppearVariants } from '../../config/framer-animations';
@@ -36,8 +44,16 @@ type ContentListItemType = {
 	itemDescription: string;
 };
 
+enum ScreenSize {
+	SMALL,
+	MEDIUM,
+}
+
+const swipeConfidenceThreshold = 10000;
+const swipePower = (offset: number, velocity: number): number =>
+	Math.abs(offset) * velocity;
+
 const HorizontalAccordian = ({ sections }: HAProps): JSX.Element => {
-	const [ref, inView] = useInView();
 	const [open, setOpen] = useState(false);
 	const [activeIndex, setActiveIndex] = useState<number | null>(null);
 
@@ -46,65 +62,209 @@ const HorizontalAccordian = ({ sections }: HAProps): JSX.Element => {
 		setOpen(false);
 	};
 
-	useEffect(() => {
-		if (!inView) {
-			closeMenu();
+	const [direction, setDirection] = useState(0);
+
+	const paginate = (newDirection: -1 | 1): void => {
+		setDirection(newDirection);
+
+		if (activeIndex !== null) {
+			if (newDirection === -1)
+				setActiveIndex((activeIndex - 1 + sections.length) % sections.length);
+			if (newDirection === 1)
+				setActiveIndex((activeIndex + 1) % sections.length);
 		}
-	}, [inView]);
+	};
 
 	return (
-		<Media queries={{ bigScreen: device.laptopL, smallScreen: device.tablet }}>
-			{(matches) => (
-				<MainContainer aria-label="main-container" ref={ref}>
-					<SectionsMenu
-						aria-label="sections-menu"
-						noOfChildren={sections.length}
+		<Media
+			queries={{ mediumScreen: device.laptopL, smallScreen: device.laptop }}
+		>
+			{(matches) => {
+				const currentScreenSize: ScreenSize | null = matches.smallScreen
+					? ScreenSize.SMALL
+					: matches.mediumScreen
+					? ScreenSize.MEDIUM
+					: null;
+				const Arrow = ({
+					children,
+					sticky,
+					onClick,
+				}: {
+					children: React.ReactNode;
+					sticky: 'left' | 'right';
+					onClick: React.MouseEventHandler<HTMLDivElement>;
+				}): JSX.Element => (
+					<ArrowNavigation
+						aria-label={`${sticky}-arrow`}
+						sticky={sticky}
+						animate={{ filter: 'brightness(70%)' }}
+						whileHover={{ filter: 'brightness(100%)', scale: 1.1 }}
+						transition={{
+							duration: 0.5,
+							type: 'easeIn',
+						}}
+						onClick={onClick}
 					>
-						{activeIndex !== null && <StyledBackdrop onClick={closeMenu} />}
-						{sections.map((experience, i) => (
-							<Section
-								sectionInfo={experience}
-								key={experience.title}
-								handleClick={() => {
-									if (activeIndex === i) {
-										setActiveIndex(null);
-										setOpen(false);
-									} else {
-										setActiveIndex(i);
-										setOpen(true);
-									}
-								}}
-								active={i === activeIndex}
-								focused={open}
-								shiftLeft={activeIndex != null && i < activeIndex}
-								isLast={i >= Math.floor((sections.length + 1) / 2)}
-							/>
-						))}
-					</SectionsMenu>
-					<AnimatePresence>
-						{matches.smallScreen && activeIndex !== null && (
-							<OuterSectionContent
-								aria-label="section-content"
-								active={open}
-								variants={animateAppearVariants}
-								initial="initial"
-								exit="initial"
-								animate={open ? 'animate' : 'initial'}
+						{children}
+					</ArrowNavigation>
+				);
+				return (
+					<AnimateSharedLayout>
+						<MainContainer aria-label="main-container">
+							<SectionsMenu
+								aria-label="sections-menu"
+								noOfChildren={sections.length}
+								smallScreen
 							>
-								<h2 className="title-main">{sections[activeIndex].title}</h2>
-								{sections[activeIndex].subtitle && (
-									<h3 className="title-sub">
-										{sections[activeIndex].subtitle}
-									</h3>
-								)}
-								<SectionContentList
-									sectionContent={sections[activeIndex].contentListItems}
-								/>
-							</OuterSectionContent>
-						)}
-					</AnimatePresence>
-				</MainContainer>
-			)}
+								{currentScreenSize === ScreenSize.SMALL &&
+									activeIndex !== null && (
+										<>
+											<Arrow sticky="left" onClick={() => paginate(-1)}>
+												<IoCaretBackOutline />
+											</Arrow>
+
+											<AnimatePresence custom={direction} initial={false}>
+												<SSSectionContainer
+													key={activeIndex}
+													aria-label="section-container"
+													custom={direction}
+													variants={{
+														// eslint-disable-next-line @typescript-eslint/no-shadow
+														enter: (direction: number) => ({
+															x: direction > 0 ? 1000 : -1000,
+															opacity: 0,
+														}),
+														center: {
+															zIndex: 99,
+															x: 0,
+															opacity: 1,
+														},
+														// eslint-disable-next-line @typescript-eslint/no-shadow
+														exit: (direction: number) => ({
+															x: direction < 0 ? 1000 : -1000,
+															opacity: 0,
+															transition: { duration: 0.1 },
+														}),
+													}}
+													initial="enter"
+													animate="center"
+													exit="exit"
+													transition={{
+														x: { type: 'spring', stiffness: 300, damping: 30 },
+														opacity: { duration: 0.2 },
+													}}
+													drag="x"
+													dragConstraints={{ left: 0, right: 0 }}
+													dragElastic={1}
+													onDragEnd={(e, { offset, velocity }) => {
+														const swipe = swipePower(offset.x, velocity.x);
+
+														if (swipe < -swipeConfidenceThreshold) {
+															paginate(1);
+														} else if (swipe > swipeConfidenceThreshold) {
+															paginate(-1);
+														}
+													}}
+												>
+													<SSSectionImageContainer
+														aria-label="section-image-container"
+														aspectRatio={0.75}
+													>
+														<SectionImageOutter aria-label="section-image-outter">
+															<SectionImage
+																src={sections[activeIndex].image}
+																aria-label="section-image"
+																variants={{
+																	animate: {
+																		height: '100%',
+																		width: '100%',
+																		opacity: 0.75,
+																	},
+																	hover: { opacity: 1 },
+																}}
+																animate="animate"
+																initial="animate"
+																whileHover="hover"
+																transition={{
+																	duration: 0.5,
+																	type: 'easeIn',
+																}}
+															/>
+														</SectionImageOutter>
+													</SSSectionImageContainer>
+												</SSSectionContainer>
+											</AnimatePresence>
+											<Arrow sticky="right" onClick={() => paginate(1)}>
+												<IoCaretForwardOutline />
+											</Arrow>
+											<CloseButtonContainer
+												onClick={() => closeMenu()}
+												animate={{ filter: 'brightness(70%)' }}
+												whileHover={{ filter: 'brightness(100%)', scale: 1.2 }}
+												transition={{
+													duration: 0.5,
+													type: 'easeIn',
+												}}
+											>
+												<IoCloseSharp size={20} />
+											</CloseButtonContainer>
+										</>
+									)}
+								{!(matches.smallScreen && open) &&
+									sections.map((experience, i) => (
+										<Section
+											sectionInfo={experience}
+											key={experience.title}
+											handleClick={() => {
+												if (activeIndex === i) {
+													setActiveIndex(null);
+													setOpen(false);
+												} else {
+													setActiveIndex(i);
+													setOpen(true);
+												}
+											}}
+											active={i === activeIndex}
+											focused={open}
+											shiftLeft={activeIndex != null && i < activeIndex}
+											isLast={i >= Math.floor((sections.length + 1) / 2)}
+											screenSize={currentScreenSize}
+										/>
+									))}
+							</SectionsMenu>
+							<AnimatePresence>
+								{currentScreenSize === ScreenSize.SMALL &&
+									activeIndex !== null && (
+										<OuterSectionContent
+											aria-label="section-content"
+											active={open}
+											variants={animateAppearVariants}
+											initial="initial"
+											exit="initial"
+											animate={open ? 'animate' : 'initial'}
+											transition={{
+												duration: 0.5,
+												type: 'easeIn',
+											}}
+										>
+											<h2 className="title-main">
+												{sections[activeIndex].title}
+											</h2>
+											{sections[activeIndex].subtitle && (
+												<h3 className="title-sub">
+													{sections[activeIndex].subtitle}
+												</h3>
+											)}
+											<SectionContentList
+												sectionContent={sections[activeIndex].contentListItems}
+											/>
+										</OuterSectionContent>
+									)}
+							</AnimatePresence>
+						</MainContainer>
+					</AnimateSharedLayout>
+				);
+			}}
 		</Media>
 	);
 };
@@ -116,6 +276,7 @@ const Section = ({
 	focused,
 	shiftLeft,
 	isLast,
+	screenSize,
 }: {
 	sectionInfo: HASectionType;
 	handleClick: () => void;
@@ -123,134 +284,150 @@ const Section = ({
 	focused: boolean;
 	shiftLeft: boolean;
 	isLast: boolean;
+	screenSize: ScreenSize | null;
 }): JSX.Element => {
 	const { image, title, shortenedTitle, subtitle, contentListItems } =
 		sectionInfo;
-	console.log(title, active);
+
+	const imageAnimationsProps = {
+		variants: {
+			animateInactive: {
+				width: '200%',
+				height: '200%',
+				opacity: 0.7,
+			},
+			animateInactiveFocused: {
+				width: '200%',
+				height: '200%',
+				opacity: 0.25,
+			},
+			animateActive: {
+				height: '100%',
+				width: '100%',
+				opacity: 0.75,
+			},
+			hover: {
+				opacity: 1,
+			},
+		},
+		animate: active
+			? 'animateActive'
+			: focused
+			? 'animateInactiveFocused'
+			: 'animateInactive',
+		initial: 'animateInactive',
+	};
+
+	const sectionAnimationProps = {
+		variants: {
+			initial: {
+				x: 0,
+			},
+			animateLeft: {
+				x: '-100%',
+			},
+			animateRight: {
+				x: '100%',
+			},
+		},
+		animate:
+			!focused || active
+				? 'initial'
+				: shiftLeft
+				? 'animateLeft'
+				: 'animateRight',
+		transition: {
+			duration: 0.5,
+			type: 'easeIn',
+		},
+	};
+
 	return (
-		<Media queries={{ bigScreen: device.laptopL, smallScreen: device.tablet }}>
-			{(matches) => (
-				<SectionContainer
-					aria-label="section-container"
-					active={active}
-					shiftLeft={shiftLeft}
-					focused={focused}
-					variants={{
-						initial: {
-							x: 0,
-						},
-						animateLeft: {
-							x: '-100%',
-						},
-						animateRight: {
-							x: '100%',
-						},
-					}}
-					animate={
-						!focused || active
-							? 'initial'
-							: shiftLeft
-							? 'animateLeft'
-							: 'animateRight'
-					}
-					transition={{
-						duration: 0.5,
-						type: 'easeIn',
-					}}
-				>
-					<AnimatePresence>
-						{!matches.smallScreen && active && (
-							<SectionContent
-								aria-label="section-content"
-								active={active}
-								isLast={isLast}
-								variants={{
-									animate: {
-										opacity: 1,
-									},
-									initialLeft: {
-										opacity: 0,
-										x: '-100%',
-									},
-									initialRight: {
-										opacity: 0,
-										x: '100%',
-									},
-								}}
-								initial={isLast ? 'initialLeft' : 'initialRight'}
-								exit="initial"
-								animate={active ? 'animate' : 'initial'}
-								transition={{
-									duration: 0.5,
-									type: 'easeIn',
-									delay: 0.2,
-								}}
-							>
-								<h2 className="title-main">{title}</h2>
-								{subtitle && <h3 className="title-sub">{subtitle}</h3>}
-								<SectionContentList sectionContent={contentListItems} />
-							</SectionContent>
-						)}
-					</AnimatePresence>
-					<SectionImageContainer
-						aria-label="section-image-container"
-						aspectRatio={0.75}
-						onClick={() => handleClick()}
+		<SectionContainer
+			aria-label="section-container"
+			active={active}
+			shiftLeft={shiftLeft}
+			focused={focused}
+			// eslint-disable-next-line react/jsx-props-no-spreading
+			{...sectionAnimationProps}
+		>
+			<AnimatePresence>
+				{active && (
+					<SectionContent
+						aria-label="section-content"
+						active={active}
+						isLast={isLast}
+						variants={{
+							animate: {
+								opacity: 1,
+							},
+							initialLeft: {
+								opacity: 0,
+								x: '-100%',
+							},
+							initialRight: {
+								opacity: 0,
+								x: '100%',
+							},
+						}}
+						initial={isLast ? 'initialLeft' : 'initialRight'}
+						exit="initial"
+						animate={active ? 'animate' : 'initial'}
+						transition={{
+							duration: 0.5,
+							type: 'easeIn',
+							delay: 0.2,
+						}}
 					>
-						<SectionImageOutter aria-label="section-image-outter">
-							<SectionImage
-								src={image}
-								aria-label="section-image"
-								variants={{
-									animateInactive: {
-										width: '200%',
-										height: '200%',
-										opacity: '0.7',
-									},
-									animateInactiveFocused: {
-										width: '200%',
-										height: '200%',
-										opacity: '0.25',
-									},
-									animateActive: {
-										height: '100%',
-										width: '100%',
-										opacity: '0.75',
-									},
-									hover: {
-										opacity: '1.0',
-									},
-								}}
-								animate={
-									active
-										? 'animateActive'
-										: focused
-										? 'animateInactiveFocused'
-										: 'animateInactive'
-								}
-								whileHover="hover"
-								transition={{
-									duration: 0.5,
-									type: 'easeIn',
-								}}
-							/>
-						</SectionImageOutter>
-					</SectionImageContainer>
-					<SectionName
-						aria-label="section-name"
-						variants={animateAppearVariants}
-						initial="initial"
-						animate={active ? 'initial' : 'animate'}
+						<h2 className="title-main">{title}</h2>
+						{subtitle && <h3 className="title-sub">{subtitle}</h3>}
+						<SectionContentList sectionContent={contentListItems} />
+					</SectionContent>
+				)}
+			</AnimatePresence>
+			<SectionImageContainer
+				aria-label="section-image-container"
+				aspectRatio={0.75}
+			>
+				<SectionImageOutter aria-label="section-image-outter">
+					<SectionImage
+						src={image}
+						aria-label="section-image"
+						// eslint-disable-next-line react/jsx-props-no-spreading
+						{...imageAnimationsProps}
+						whileHover="hover"
+						onTap={handleClick}
 						transition={{
 							duration: 0.5,
 							type: 'easeIn',
 						}}
-					>
-						<h6>{matches.bigScreen ? shortenedTitle : title}</h6>
-					</SectionName>
-				</SectionContainer>
-			)}
-		</Media>
+					/>
+				</SectionImageOutter>
+			</SectionImageContainer>
+			<SectionName
+				aria-label="section-name"
+				variants={{
+					animate: {
+						opacity: 1,
+					},
+					initial: {
+						opacity: 0,
+					},
+				}}
+				initial="initial"
+				animate={active || focused ? 'initial' : 'animate'}
+				transition={{
+					duration: 0.5,
+					type: 'easeIn',
+				}}
+			>
+				<h6>
+					{screenSize === ScreenSize.MEDIUM || screenSize === ScreenSize.SMALL
+						? shortenedTitle
+						: title}
+				</h6>
+			</SectionName>
+		</SectionContainer>
 	);
 };
 
--- src/components/HorizontalAccordion/HorizontalAccordianStyles.ts
+++ src/components/HorizontalAccordion/HorizontalAccordianStyles.ts
@@ -3,16 +3,7 @@ import styled, { css } from 'styled-components';
 import { motion } from 'framer-motion';
 import { device } from '../../config/display';
 
-export const StyledBackdrop = styled.div`
-	z-index: 2;
-	position: fixed;
-	top: 0;
-	width: 100vw;
-	height: 100vh;
-	backdrop-filter: blur(2px);
-`;
-
-export const MainContainer = styled.div`
+export const MainContainer = styled(motion.div)`
 	display: flex;
 	flex-wrap: wrap;
 	padding: 3rem 20rem;
@@ -39,6 +30,13 @@ export const SectionName = styled(motion.div)`
 	}
 `;
 
+export const SSSectionContainer = styled(motion.div)`
+	flex: 0 0 20%;
+	position: relative;
+	display: flex;
+	z-index: 99;
+`;
+
 export const SectionContainer = styled(motion.div)<{
 	active: boolean;
 	shiftLeft: boolean;
@@ -54,6 +52,7 @@ export const SectionContainer = styled(motion.div)<{
 		${SectionName} {
 			${({ theme }) =>
 				css`
+					transition: border-color 500ms ease, color 500ms ease;
 					border-color: ${theme.fontColours.main};
 					color: ${theme.fontColours.main};
 				`}
@@ -67,15 +66,39 @@ export const SectionContainer = styled(motion.div)<{
 		`}
 `;
 
-export const SectionsMenu = styled.div<{ noOfChildren: number }>`
-	flex: 1;
+export const CloseButtonContainer = styled(motion.div)`
+	position: absolute;
+	cursor: pointer;
+	pointer-events: all;
+	z-index: 99;
+	user-select: none;
+	top: -4rem;
+	right: 5px;
+	padding: 1rem;
+`;
+
+export const ArrowNavigation = styled(motion.div)<{ sticky: 'left' | 'right' }>`
+	position: absolute;
+	cursor: pointer;
+	pointer-events: all;
+	z-index: 99;
+	font-weight: bold;
+	font-size: 2rem;
 	display: flex;
-	justify-content: center;
+	align-items: center;
+	user-select: none;
 	box-sizing: border-box;
-	align-items: stretch;
-	${SectionContainer} {
-		flex: ${({ noOfChildren }) => `0 0 calc(100% / ${noOfChildren})`};
-	}
+	top: 50%;
+	${({ sticky }) =>
+		sticky === 'left'
+			? css`
+					left: 0;
+					padding-right: 4rem;
+			  `
+			: css`
+					right: 0;
+					padding-left: 4rem;
+			  `}
 `;
 
 export const ContentListContainer = styled.div`
@@ -115,7 +138,45 @@ export const SectionImageOutter = styled.div`
 `;
 
 export const SectionImage = styled(motion.img)`
-	cursor: pointer;
+	/* cursor: pointer; */
+	/* pointer-events: auto; */
+	user-select: none;
+`;
+
+export const SSSectionImageContainer = styled(SectionImageContainer)`
+	${SectionImageOutter} {
+		${SectionImage} {
+			pointer-events: none;
+		}
+	}
+`;
+
+export const SectionsMenu = styled(motion.div)<{
+	noOfChildren: number;
+	smallScreen?: boolean;
+}>`
+	flex-basis: 100%;
+	display: flex;
+	justify-content: center;
+	box-sizing: border-box;
+	align-items: stretch;
+	position: relative;
+	${SectionContainer} {
+		flex: ${({ noOfChildren }) => `0 0 calc(100% / ${noOfChildren})`};
+	}
+	${({ smallScreen }) =>
+		smallScreen &&
+		css`
+			&::before {
+				content: '';
+				position: absolute;
+				top: 0;
+				left: 0;
+				width: 100%;
+				height: 100%;
+				z-index: 3;
+			}
+		`}
 `;
 
 const SharedSectionContent = styled(motion.div)<{ active: boolean }>`
@@ -135,10 +196,13 @@ const SharedSectionContent = styled(motion.div)<{ active: boolean }>`
 		margin-bottom: 2rem;
 	}
 	z-index: 99;
+	pointer-events: none;
 `;
 
 export const OuterSectionContent = styled(SharedSectionContent)`
 	flex: 0 0 100%;
+	margin-top: 2rem;
+	pointer-events: all;
 	${ContentListContainer} {
 		text-align: justify;
 	}
